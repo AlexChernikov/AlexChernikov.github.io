46412b17729b418240e44b2a812dc59cbedb9734
avchernikov <avchernikov@phoenixit.ru>
2020-09-27 19:26:15 +0300

    commit

---
+++ vanta-scripts/net/custom-back-ground.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#custom-back-ground",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff,
+  backgroundColor: 0x2002f,
+  points: 20.00,
+  maxDistance: 30.00,
+  spacing: 20.00
+})
