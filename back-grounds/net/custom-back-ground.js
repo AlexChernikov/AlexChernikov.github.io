b3855e83a46f360e1878278f76c0846b049de29f
avchernikov <avchernikov@phoenixit.ru>
2020-10-01 02:19:12 +0300

    alpha

--- back-grounds/net/custom-back-ground.js
+++ back-grounds/net/custom-back-ground.js
@@ -7,8 +7,8 @@ VANTA.NET({
   minWidth: 200.00,
   scale: 1.00,
   scaleMobile: 1.00,
-  color: 0xff,
-  backgroundColor: 0x2002f,
+  color: 0x88,
+  backgroundColor: 0x10018,
   points: 20.00,
   maxDistance: 30.00,
   spacing: 20.00
